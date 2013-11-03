7a1815114744991345a1a488fae5171e25d84fa4
ckw2013 <marketing.guru1@gmail.com>
2013-11-04 11:28:19 +1300

    Update spree_html_invoice.rb

--- lib/spree_html_invoice.rb
+++ lib/spree_html_invoice.rb
@@ -1,3 +1,10 @@
 require 'spree/core'
 require 'spree_html_invoice/engine'
 
+module Spree
+  module HtmlInvoice
+    def self.config(&block)
+      yield(Spree::HtmlInvoice::Config)
+    end
+  end
+end
